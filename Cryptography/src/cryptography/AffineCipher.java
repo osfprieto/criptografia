0ddb1006d343b843f999536128131bffafa8dcf7
Miguel <Miguel@Miguel-PC>
2015-09-06 20:23:56 -0500

    Interfaz de Usuario. Crifrado y Descrifrado con el Criptosistema por Sustitución.

---
+++ Cryptography/src/cryptography/AffineCipher.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptography;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Miguel
+ */
+public class AffineCipher {
+    
+    public static String encryptionMessage(String Msg)
+    {
+        String CTxt = "";
+        int a = 3;
+        int b = 6;
+        for (int i = 0; i < Msg.length(); i++)
+        {
+            CTxt = CTxt + (char) ((((a * Msg.charAt(i)) + b) % 26) + 65);
+        }
+        return CTxt;
+    }
+ 
+    public static String decryptionMessage(String CTxt)
+    {
+        String Msg = "";
+        int a = 3;
+        int b = 6;
+        int a_inv = 0;
+        int flag;
+        for (int i = 0; i < 26; i++)
+        {
+            flag = (a * i) % 26;
+            if (flag == 1)
+            {
+                a_inv = i;
+                System.out.println(i);
+            }
+        }
+        for (int i = 0; i < CTxt.length(); i++)
+        {
+            Msg = Msg + (char) (((a_inv * ((CTxt.charAt(i) - b)) % 26)) + 65);
+        }
+        return Msg;
+    }
+ 
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the message: ");
+        String message = sc.next();
+        System.out.println("Message is :" + message);
+        System.out.println("Encrypted Message is : "
+                + encryptionMessage(message));
+        System.out.println("Decrypted Message is: "
+                + decryptionMessage(encryptionMessage(message)));
+        sc.close();
+    }
+}
---
+++ Cryptography/src/cryptography/MonoalphabeticCipher.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cryptography;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Miguel
+ */
+public class MonoalphabeticCipher {
+    
+    private String plainText;
+    private String cipherText;
+    private String key;
+    
+    public MonoalphabeticCipher(){}
+    
+    public static char p[]  = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+            'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+            'w', 'x', 'y', 'z' };
+    
+    public static char ch[] = { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O',
+            'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C',
+            'V', 'B', 'N', 'M' };
+ 
+    public static String doEncryption(String s)    {
+        char c[] = new char[(s.length())];
+        for (int i = 0; i < s.length(); i++){
+            for (int j = 0; j < 26; j++){
+                if (p[j] == s.charAt(i)){
+                    c[i] = ch[j];
+                    break;
+                }
+            }
+        }
+        return (new String(c));
+    }
+ 
+    public static String doDecryption(String s){
+        char p1[] = new char[(s.length())];
+        for (int i = 0; i < s.length(); i++){
+            for (int j = 0; j < 26; j++){
+                if (ch[j] == s.charAt(i)){
+                    p1[i] = p[j];
+                    break;
+                }
+            }
+        }
+        return (new String(p1));
+    }
+    public void encrypt(){
+        setCipherText(doEncryption(getPlainText().toLowerCase()));
+    }
+    
+    public void decryp(){
+        setPlainText(doDecryption(getCipherText().toUpperCase()));
+    }
+
+    public String getPlainText() {
+        return plainText;
+    }
+
+    public void setPlainText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    public String getCipherText() {
+        return cipherText;
+    }
+
+    public void setCipherText(String cipherText) {
+        this.cipherText = cipherText;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    } 
+}
